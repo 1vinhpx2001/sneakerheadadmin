ca176fe8c783c42ca70941ede201085cacb3054d
Xuan Vinh <19110011@student.hcmute.edu.vn>
2023-12-19 10:42:19 +0700

    deploy

--- src/common/const.jsx
+++ src/common/const.jsx
@@ -1,4 +1,4 @@
-export const API_HOST= "https://railway.app/project/1184ee36-fdd7-4ca0-9b15-0c29bb0b49d2/api"
+export const API_HOST= "https://sneakerheadstore-5968ae1f44cc.herokuapp.com/api"
 export const API_LOCAL= "http://localhost:8080/api"
 export const API_ADDRESS = "https://online-gateway.ghn.vn/shiip/public-api/"
 export const TOKEN_API_ADDRESS = "5eff91b7-6108-11ee-8bfa-8a2dda8ec551"
