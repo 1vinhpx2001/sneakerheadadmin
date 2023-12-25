99c87030696b64e40bb327e12b09719289b45b9c
Xuan Vinh <19110011@student.hcmute.edu.vn>
2023-12-25 15:01:49 +0700

    deploy

--- src/common/const.jsx
+++ src/common/const.jsx
@@ -1,4 +1,4 @@
-export const API_HOST= "https://sneaker-store-backend-production.up.railway.app/api"
+export const API_HOST= "https://sneakerheadstore-5968ae1f44cc.herokuapp.com/api"
 export const API_LOCAL= "http://localhost:8080/api"
 export const API_ADDRESS = "https://online-gateway.ghn.vn/shiip/public-api/"
 export const TOKEN_API_ADDRESS = "5eff91b7-6108-11ee-8bfa-8a2dda8ec551"
